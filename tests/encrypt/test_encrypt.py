b06f693dd1da1e6be6261a0c45febe3e41a44a59
Matheus <stvgamer74@gmail.com>
2022-12-03 20:37:28 -0300

    fix: :bug: add a blank line

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,6 +1,7 @@
 from challenges.challenge_encrypt_message import encrypt_message
 import pytest
 
+
 def test_encrypt_message():
     assert encrypt_message("AABBCC", 3) == "BAA_CCB"
     assert encrypt_message("ABBCCA", 4) == "AC_CBBA"
