5a0267ddd38569e618073f910641ed37174bdda2
Matheus <stvgamer74@gmail.com>
2022-12-03 15:28:04 -0300

    feat: :sparkles: implements a function study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(target_time, int):
+        return None
+    count = 0
+    for element in permanence_period:
+        join_time = element[0]
+        exit_time = element[1]
+        if not isinstance(join_time, int) or not isinstance(exit_time, int):
+            return None
+        if join_time <= target_time <= exit_time:
+            count += 1
+    return count
