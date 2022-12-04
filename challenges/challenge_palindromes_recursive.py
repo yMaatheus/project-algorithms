262a7fc4c7b63b347bd76f4b6e23122129074a2c
Matheus <stvgamer74@gmail.com>
2022-12-03 21:29:28 -0300

    fix: :bug: fix check word in the function is_palindrome_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,5 +1,5 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    if not word or word[low_index] != word[high_index]:
+    if len(word) == 0 or word[low_index] != word[high_index]:
         return False
     # Base case:
     if low_index >= high_index:
