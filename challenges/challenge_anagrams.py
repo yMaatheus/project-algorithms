c81f2b7d0964ce21e45e5d0bcd3d7a742929a30d
Matheus <stvgamer74@gmail.com>
2022-12-04 07:52:21 -0300

    feat: :sparkles: implements a function is_anagram with python sorted

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,9 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    is_anagram = True
+    first_string_sorted = ''.join(sorted(first_string))
+    second_string_sorted = ''.join(sorted(second_string))
+    for char in first_string:
+        if char.lower() not in map(str.lower, second_string):
+            is_anagram = False
+    print((first_string_sorted, second_string_sorted, is_anagram))
+    return (first_string_sorted, second_string_sorted, is_anagram)
