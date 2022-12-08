bda9aad8b21ad45acafbdbce7df4906a274a24a5
Matheus <stvgamer74@gmail.com>
2022-12-07 23:25:44 -0300

    feat: :sparkles: implements the functions quick_sort and partition in challenge_anagrams and refactor the function is_anagram

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,9 +1,31 @@
 def is_anagram(first_string, second_string):
-    is_anagram = True
-    first_string_sorted = ''.join(sorted(first_string))
-    second_string_sorted = ''.join(sorted(second_string))
-    for char in first_string:
-        if char.lower() not in map(str.lower, second_string):
-            is_anagram = False
-    print((first_string_sorted, second_string_sorted, is_anagram))
+    first_string_sorted = ''.join(quick_sort(list(first_string.lower())))
+    second_string_sorted = ''.join(quick_sort(list(second_string.lower())))
+    is_anagram = (False if first_string == '' or second_string == ''
+                  else True if first_string_sorted == second_string_sorted
+                  else False)
     return (first_string_sorted, second_string_sorted, is_anagram)
+
+
+def quick_sort(arr, start=0, end=None):
+    if end is None:
+        end = len(arr) - 1
+    if start < end:
+        pivot = partition(arr, start, end)
+        quick_sort(arr, start, pivot - 1)
+        quick_sort(arr, pivot + 1, end)
+    return arr
+
+
+def partition(arr, start, end):
+    pivot = arr[end]
+    delimiter = start - 1
+
+    for index in range(start, end):
+        if arr[index] <= pivot:
+            delimiter = delimiter + 1
+            arr[index], arr[delimiter] = arr[delimiter], arr[index]
+
+    arr[delimiter + 1], arr[end] = arr[end], arr[delimiter + 1]
+
+    return delimiter + 1
