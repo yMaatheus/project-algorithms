3841701a281553e1435da2431b68b4445741ebe7
Matheus <stvgamer74@gmail.com>
2022-12-08 01:12:55 -0300

    feat: :sparkles: implements the function is_palindrome_iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,12 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0:
+        return False
+    mid = len(word) // 2
+    count = 0
+    while count <= mid:
+        start = word[count]
+        end = word[(len(word) - 1) - count]
+        if (start != end):
+            return False
+        count += 1
+    return True
