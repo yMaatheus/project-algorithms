8e0b7368c9fb51f4f4b5ac76097217a78aa12539
Matheus <stvgamer74@gmail.com>
2022-12-08 00:08:31 -0300

    feat: :sparkles: implements the function find_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,11 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    nums.sort()
+    if len(nums) <= 1:
+        return False
+    for index in range(len(nums)-1):
+        num = nums[index]
+        if not isinstance(num, int) or num <= 0:
+            return False
+        if num == nums[index + 1]:
+            return num
+    return False
